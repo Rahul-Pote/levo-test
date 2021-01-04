da64ecffe0de76017b14332d488d9aa04a84e14f
Rahul Pote <rpote4055@gmail.com>
2021-01-04 17:35:49 +0530

    Add Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import { Button } from 'react-bootstrap';
 
 import './style/index.scss';
 
 function App() {
-  return <div className="App">Hello Word!!!</div>;
+  return (
+    <div className="App">
+      <Button variant="primary">Primary</Button>
+    </div>
+  );
 }
 
 export default App;
